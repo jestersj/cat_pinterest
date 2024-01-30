79eca8f03eca74b096e86086310b81f7e18361f0
jestersj <valuevmp@yandex.ru>
2024-01-30 15:37:27 +0300

    initial commit

--- src/app/_http/catsApi.ts
+++ src/app/_http/catsApi.ts
@@ -2,7 +2,7 @@ import {$host} from "@/app/_http/index";
 import {ICat} from "@/app/_types/ICat";
 
 export const fetchCats = async (page: number) => {
-    const {data} = await $host.get<ICat[]>(`/images/search?limit=52&page=${page}`, {
+    const {data} = await $host.get<ICat[]>(`/images/search?limit=32&page=${page}`, {
         headers: {'x-api-key': 'live_Cgzng6UBSpZMeIm7PXyT03zle3oH8szVKuPeYsPVdoV8Oi4L3WhqPTfUIl7tw5Ws'}
     })
     return data
